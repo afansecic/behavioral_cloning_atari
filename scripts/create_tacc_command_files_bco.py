2d3b8dcf799c4dec3c1aa87d7fa8f650a6b7fb70
dsbrown1331 <dsbrown1331@gmail.com>
2019-01-18 15:23:13 -0600

    tacc

---
+++ scripts/create_tacc_command_files_bco.py
@@ -0,0 +1,15 @@
+def create_command_file(envname):
+    command_str = "module load tacc-singularity\n"
+    command_str += 'singularity exec --nv ${SINGULARITY_CACHEDIR}/tacc-maverick-ml-latest.simg python -c "import torch; print(torch.cuda.is_available())"\n'
+    command_str += "singularity exec --nv ${SINGULARITY_CACHEDIR}/tacc-maverick-ml-latest.simg python main_bco.py --env_name " + envname + " --num_transitions 20000\n"
+    print(command_str)
+    f = open("commands_" + envname,'w')
+    f.write(command_str)
+    f.close()
+    
+envs = ['mspacman', 'videopinball', 'hero', 'beamrider', 'qbert', 'seaquest', 'breakout', 'spaceinvaders', 'pong', 'enduro' ]
+for e in envs:
+    print("+"*20)
+    print(e)
+    print("+"*20)
+    create_command_file(e)
